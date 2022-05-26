498992ad375a556244f8ab99da47fdba8023dd20
Brandon Noble <51976754+brlnoble@users.noreply.github.com>
2022-05-26 11:52:31 -0400

    Now reads all 6 TC and records to CSV on network

--- Raspberry Pi/tcRead.py
+++ Raspberry Pi/tcRead.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from time import time
+import datetime
+import csv
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -8,8 +11,9 @@ TC1 = 16
 TC2 = 18
 TC3 = 22
 TC4 = 32
+TC5 = 11
 TC6 = 36
-tcList = [TC1, TC2, TC3, TC4, TC6] #array of inputs
+tcList = [TC1,TC2,TC3,TC4,TC5,TC6] #array of inputs
 
 #Initialize pin for amplifer controls
 CS = 29
@@ -17,36 +21,48 @@ SCK = 31
 
 GPIO.setup(tcList, GPIO.IN) #Setup inputs
 GPIO.setup([CS,SCK], GPIO.OUT) #Setup outputs
+GPIO.output(CS,GPIO.HIGH) #start chip select as high
 
 #Array for outputs
-tcRead  =[0,0,0,0,0]
+tcRead  =['','0','0','0','0','0','0']
 
-def readTC(TC):
-	read = 0
-	GPIO.output(SCK,GPIO.HIGH) #See if this fixes things
-	GPIO.output(CS,GPIO.LOW)	
+#Variables
+path ='/diskstation1/share/1 - Mill/DATA/Brandon Stuff/CODE/' 
 
-	for j in range(15,-1,-1):
-		GPIO.output(SCK,GPIO.LOW)
-		sleep(0.01)
-		read |= (GPIO.input(TC) << j)
-		GPIO.output(SCK,GPIO.HIGH)
-		sleep(0.01)
+def readTC(tcList):
+	for i in range(0,len(tcList)):
+		read = 0
+		GPIO.output(SCK,GPIO.HIGH) #See if this fixes things
+		GPIO.output(CS,GPIO.LOW)
+	 
+		for j in range(15,-1,-1):
+			GPIO.output(SCK,GPIO.LOW)
+			sleep(0.01)
+			read |= (GPIO.input(tcList[i]) << j)
+			GPIO.output(SCK,GPIO.HIGH)
+			sleep(0.01)
 
-	GPIO.output(CS,GPIO.HIGH)
-	return read
+		GPIO.output(CS,GPIO.HIGH)
 
+		#Convert to Fahrenheit
+		read >>= 5
+		read *= 9/5
+		read += 32
+		tcRead[i+1] = f'{read:.2f}'
 
-#Only checking if the one thermocouple works so far
-reading = readTC(TC4)
-print('Reading: ' + str(reading))
-reading >>= 3
-print('R-Shift 3: ' + str(reading))
-reading *= 0.25
-print('1/4 R: ' + str(reading))
-reading *= 9/5
-reading += 32
-print(f'Fahrenheit: {reading:.2f}')
+currTime=datetime.datetime.fromtimestamp(time()).strftime("%d %B, %Y - %I:%M:%S %p")
+tcRead[0] = currTime 
+#Check if the thing works lol
+readTC(tcList)
 
+print('----- '+currTime+' -----')
+for i in range(0,len(tcRead)):
+	print(tcRead[i])
+
+#Write to CSV log
+with open(path+'OnlineLog.csv','a',newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow(tcRead)
+
+#Clear the GPIO so they are not in use
 GPIO.cleanup()
-
