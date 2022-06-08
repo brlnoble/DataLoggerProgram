0f1db44ba11c106c631419911bd15f40d537d527
Brandon Noble <51976754+brlnoble@users.noreply.github.com>
2022-06-08 13:02:26 -0400

    Updating from UDS-Furnace

--- plotScript.js
+++ plotScript.js
@@ -1,8 +1,24 @@
 function makeChart(csvFile) {
 
+  //Get the time column
   var timeLabels = csvFile.map(function(d) {
-    return d.Time;
-  });
+      return d.Time;
+      });
+
+
+  //If the file has not changed we don't need to refresh the window
+  console.log(timeLabels[timeLabels.length-1]);
+  console.log(document.getElementById("LastRead").textContent);
+  if(timeLabels[timeLabels.length-1] === document.getElementById("LastRead").textContent) {
+    return;
+  };
+
+  //Put the date and time on two lines for the graph
+  timeLabels = timeLabels.map(function(t) { 
+      return t.split(' - '); 
+      }); 
+  
+  //Get all the TC columns
   var tc1Data = csvFile.map(function(d) {
     return d.Temp1;
   });
@@ -22,17 +38,38 @@ function makeChart(csvFile) {
     return d.Temp6;
   })
   
-  var tcHData = new Array(tc1Data.length).fill("1300"); //make the high temp array since it is not in the file
+  //Create an array for the high temperature warning
+  var tcHData = new Array(tc1Data.length).fill("1300");
   
-
+  //Update the numeric text at the top of the screen
+  document.getElementById("TC1").textContent=tc1Data[tc1Data.length-1];
+  document.getElementById("TC2").textContent=tc2Data[tc2Data.length-1];
+  document.getElementById("TC3").textContent=tc3Data[tc3Data.length-1];
+  document.getElementById("TC4").textContent=tc4Data[tc4Data.length-1];
+  //document.getElementById("TC5").textContent=tc5Data[tc5Data.length-1];
+  document.getElementById("TC6").textContent=tc6Data[tc6Data.length-1];
+  document.getElementById("LastRead").textContent=timeLabels[timeLabels.length-1];
+  
+  
   var chart = new Chart('chart', {
     type: "line",
     options: {
       maintainAspectRatio: false,
       legend: {
         display: true
-      }
+      },
+
+      //Set the rotation of the X axis labels
+      scales: {
+            xAxes: [{
+                ticks: {
+                    maxRotation: 0,
+                    minRotation: 0
+                }
+            }]
+        }
     },
+
     data: {
       labels: timeLabels,
       datasets: [
@@ -83,21 +120,17 @@ function makeChart(csvFile) {
     }
   });
   
-  document.getElementById("TC1").textContent=tc1Data[tc1Data.length-1];
-  document.getElementById("TC2").textContent=tc2Data[tc2Data.length-1];
-  document.getElementById("TC3").textContent=tc3Data[tc3Data.length-1];
-  document.getElementById("TC4").textContent=tc4Data[tc4Data.length-1];
-  //document.getElementById("TC5").textContent=tc5Data[tc5Data.length-1];
-  document.getElementById("TC6").textContent=tc6Data[tc6Data.length-1];
-  document.getElementById("LastRead").textContent=timeLabels[timeLabels.length-1];
 }
 
 // Request data using D3
 function getCSV() { 
   d3
-    .csv("https://github.com/UDS-Intern/DataLogger/blob/026eaec82d10109ca5b0a1c5724ceda71f15be25/OnlineLog.csv?" + new Date().getTime())
+    .csv("./OnlineLog.csv?" + new Date().getTime())
     .then(makeChart);
+
+  //Run the program every 60s
   setTimeout(getCSV, 60000);
 }
 
+//Initial call
 getCSV();
